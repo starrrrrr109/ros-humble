a74c89d3bbba1affb40a5f87cc17fc96119ccd80
starrr <kmes100210@gmail.com>
2025-11-17 14:22:22 +0800

    add cpp_pubsub pkg

---
+++ ros_ws/build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/humble/humble_ws/src/cpp_pubsub
+# Build directory: /home/humble/humble_ws/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
